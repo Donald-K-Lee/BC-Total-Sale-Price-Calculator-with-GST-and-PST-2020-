cbd5bbbbef648eb1a9a60e1d54b100d0f4748176
Donald-K-Lee <51865552+Donald-K-Lee@users.noreply.github.com>
2020-02-09 21:21:34 -0800

    Add files via upload

--- BC Total Sale Price Calculator with GST and PST (2020).py
+++ BC Total Sale Price Calculator with GST and PST (2020).py
@@ -1,4 +1,4 @@
-#Title: Calculates the total cost of an item with GST and PST in B.C
+#Title: Calculates the total cost of an item with GST and PST in BC
 price = float(input("Enter the cost of this item without tax included:"))
 GST = price * 0.05 #GST rate in 2020 = 5%
 PST = price * 0.07 #PST rate in 2020 = 7%
